9ade6230d29d4d43089157fba806e428538b62b5
Vladislav Wohlrath <vladislav@wohlrath.cz>
2024-02-27 20:40:17 +0100

    secrets

--- flake.nix
+++ flake.nix
@@ -3,10 +3,8 @@
 
   inputs = {
     secrets = {
-      type = "github";
-      owner = "vladidobro";
-      repo = "secrets";
-      ref = "main";
+      type = "git";
+      url = "ssh://git@github.com/vladidobro/secrets.git";
     };
     nixpkgs = {
       type = "github";
