b0f75ba3212616d76bc442ebd0c238c971c85c05
Vladislav Wohlrath <vladislav@wohlrath.cz>
2024-02-27 21:46:35 +0100

    vim

--- home/nvim.nix
+++ home/nvim.nix
@@ -1,12 +1,21 @@
 { flake, config, pkgs, ... }:
 
 {
-  imports = [
-    flake.inputs.neovim.homeManagerModules.nixvim
-  ];
-
   home.stateVersion = "23.11";
 
   home.username = "vladidobro";
   home.homeDirectory = "/home/vladidobro";
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    plugins = with pkgs.vimPlugins [
+      { plugin = nvim-tree-lua;
+        config = ''
+        -- nvim tree
+        '';
+        type = "lua";
+      }
+    ];
+  };
 }
