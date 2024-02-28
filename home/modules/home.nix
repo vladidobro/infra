19fcf2a68dae5700d170e4c9096dca7608b3c063
Vladislav Wohlrath <vladislav@wohlrath.cz>
2024-02-28 09:26:08 +0100

    tmux

--- home/modules/home.nix
+++ home/modules/home.nix
@@ -36,6 +36,7 @@
 
 
         bind-key -T copy-mode-vi 'C-v' send -X rectangle-toggle
+        bind-key -T copy-mode-vi v send -X begin-selection
         bind-key -T copy-mode-vi V send -X select-line
         bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel 'pbcopy'  # macos
         #bind-key -T copy-mode-vi y send -X copy-selection  # linus
