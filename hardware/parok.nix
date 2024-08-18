0522bfddae45756306a5c618a01922137f37809d
Vladislav Wohlrath <vladislav@wohlrath.cz>
2024-08-18 21:03:25 +0200

    add gpu support

--- hardware/parok.nix
+++ hardware/parok.nix
@@ -51,4 +51,12 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      libvdpau-va-gl
+    ];
+  };
 }
