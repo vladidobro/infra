ee8edb2fccad5bfe076ac1e2dc4fdcbc7b824abd
Vladislav Wohlrath <vladislav.wohlrath@second-foundation.eu>
2026-01-18 23:06:17 +0100

    dev

--- flake.nix
+++ flake.nix
@@ -2,85 +2,55 @@
   description = "vladidobro system configuration";
 
   inputs = {
-    secrets = {
-      url = "git+ssh://git@github.com/vladidobro/secrets.git";
-    };
-    svatba = {
-      url = "github:jaroslavpesek/wedding-app?tag=0.5.0";
-      inputs.nixpkgs.follows = "nixpkgs-2405";
-    };
-    svatba-dashboard = {
-      url = "github:Danielwohlr/wedding_dashboard/nix";
-      # no follows, needs up-to-date uv
-      #inputs.nixpkgs.follows = "nixpkgs-2405";
-    };
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-    };
-    nixpkgs-2405 = {
-      url = "github:NixOS/nixpkgs/nixos-24.05";
-    };
-    nixpkgs-2411 = {
-      url = "github:NixOS/nixpkgs/nixos-24.11";
-    };
-    nixpkgs-2505 = {
-      url = "github:NixOS/nixpkgs/nixos-25.05";
-    };
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    };
-    nix-darwin-2505 = {
-      url = "github:LnL7/nix-darwin/nix-darwin-25.05";
-      inputs.nixpkgs.follows = "nixpkgs-2505";
-    };
-    home-manager-2405 = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs-2405";
-    };
-    home-manager-2411 = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs-2411";
-    };
-    home-manager-2505 = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs-2505";
-    };
-    agenix-2405 = {
-      url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs-2405";
-    };
-    agenix-2411 = {
-      url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs-2411";
-    };
-    nixos-mailserver-2405 = {
-      url = "gitlab:simple-nixos-mailserver/nixos-mailserver/nixos-24.05";
-      inputs.nixpkgs.follows = "nixpkgs-2405";
-    };
-    nix-index-database-2405 = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs-2405";
-    };
-    nix-index-database-2411 = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs-2411";
-    };
-    nix-index-database-2505 = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs-2505";
-    };
-    nixvim-2411 = {
-      url = "github:nix-community/nixvim/nixos-24.11";
-      inputs.nixpkgs.follows = "nixpkgs-2411";
-      inputs.home-manager.follows = "home-manager-2411";
-    };
-    nixvim-2505 = {
-      url = "github:nix-community/nixvim/nixos-25.05";
-      inputs.nixpkgs.follows = "nixpkgs-2505";
-    };
-    deploy-rs = {
-      url = "github:serokell/deploy-rs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    deploy-rs.url = "github:serokell/deploy-rs";
+
+
+    secrets.url = "git+ssh://git@github.com/vladidobro/secrets.git";
+    svatba.url = "github:jaroslavpesek/wedding-app?tag=0.5.0";
+    svatba.inputs.nixpkgs.follows = "nixpkgs-2405";
+
+    # 24.05 - kulich, parok
+    nixpkgs-2405.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager-2405.url = "github:nix-community/home-manager/release-24.05";
+    home-manager-2405.inputs.nixpkgs.follows = "nixpkgs-2405";
+    nixos-mailserver-2405.url = "gitlab:simple-nixos-mailserver/nixos-mailserver/nixos-24.05";
+    nixos-mailserver-2405.inputs.nixpkgs.follows = "nixpkgs-2405";
+
+    # 24.11 - myskus
+    nixpkgs-2411.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager-2411.url = "github:nix-community/home-manager/release-24.11";
+    home-manager-2411.inputs.nixpkgs.follows = "nixpkgs-2411";
+    nixvim-2411.url = "github:nix-community/nixvim/nixos-24.11";
+    nixvim-2411.inputs.nixpkgs.follows = "nixpkgs-2411";
+    nixvim-2411.inputs.home-manager.follows = "home-manager-2411";
+    nix-index-database-2411.url = "github:nix-community/nix-index-database";
+    nix-index-database-2411.inputs.nixpkgs.follows = "nixpkgs-2411";
+    agenix-2411.url = "github:ryantm/agenix";
+    agenix-2411.inputs.nixpkgs.follows = "nixpkgs-2411";
+
+    # 25.05 - sf
+    nixpkgs-2505.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nix-darwin-2505.url = "github:LnL7/nix-darwin/nix-darwin-25.05";
+    nix-darwin-2505.inputs.nixpkgs.follows = "nixpkgs-2505";
+    home-manager-2505.url = "github:nix-community/home-manager/release-25.05";
+    home-manager-2505.inputs.nixpkgs.follows = "nixpkgs-2505";
+    nixvim-2505.url = "github:nix-community/nixvim/nixos-25.05";
+    nixvim-2505.inputs.nixpkgs.follows = "nixpkgs-2505";
+    nix-index-database-2505.url = "github:nix-community/nix-index-database";
+    nix-index-database-2505.inputs.nixpkgs.follows = "nixpkgs-2505";
+
+    # 25.11
+    nixpkgs-2511.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nix-darwin-2511.url = "github:LnL7/nix-darwin/nix-darwin-25.11";
+    nix-darwin-2511.inputs.nixpkgs.follows = "nixpkgs-2511";
+    home-manager-2511.url = "github:nix-community/home-manager/release-25.11";
+    home-manager-2511.inputs.nixpkgs.follows = "nixpkgs-2511";
+    nixos-mailserver-2511.url = "gitlab:simple-nixos-mailserver/nixos-mailserver/nixos-25.11";
+    nixos-mailserver-2511.inputs.nixpkgs.follows = "nixpkgs-2511";
+
+    # unstable
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = inputs@{ flake-parts, ... }: 
@@ -91,6 +61,7 @@
       ./lib/deploy.nix
       ./lib/home.nix
       ./lib/vim.nix
+      ./lib/vpsfree.nix
       ./hosts/kulich.nix
       ./hosts/myskus.nix
       ./hosts/parok.nix
--- hosts/kulich.nix
+++ hosts/kulich.nix
@@ -1,78 +1,16 @@
 { inputs, self, ... }: 
 let 
 
-  home = {
-    vladidobro = {
-      enable = true;
-      minimal = true;
-      basic = true;
-      aliases = true;
-      nvim.enable = true;
-    };
-
-    home.stateVersion = "23.11";
-
-    home.username = "vladidobro";
-    home.homeDirectory = "/home/vladidobro";
-  };
-
-  vpsfree = { config, pkgs, lib, ... }:
-  with lib; let nameservers = [ "1.1.1.1" "2606:4700:4700::1111" ];
-  in {
-    networking.nameservers = mkDefault nameservers;
-    services.resolved = mkDefault { fallbackDns = nameservers; };
-    networking.dhcpcd.extraConfig = "noipv4ll";
-
-    systemd.services.systemd-sysctl.enable = false;
-    systemd.services.systemd-oomd.enable = false;
-    systemd.sockets."systemd-journald-audit".enable = false;
-    systemd.mounts = [ {where = "/sys/kernel/debug"; enable = false;} ];
-    systemd.services.rpc-gssd.enable = false;
-
-    # Due to our restrictions in /sys, the default systemd-udev-trigger fails
-    # on accessing PCI devices, etc. Override it to match only network devices.
-    # In addition, boot.isContainer prevents systemd-udev-trigger.service from
-    # being enabled at all, so add it explicitly.
-    systemd.additionalUpstreamSystemUnits = [
-      "systemd-udev-trigger.service"
-    ];
-    systemd.services.systemd-udev-trigger.serviceConfig.ExecStart = [
-      ""
-      "-udevadm trigger --subsystem-match=net --action=add"
-    ];
-
-    boot.isContainer = true;
-    boot.enableContainers = mkDefault true;
-    boot.loader.initScript.enable = true;
-    boot.specialFileSystems."/run/keys".fsType = mkForce "tmpfs";
-    boot.systemdExecutable = mkDefault "/run/current-system/systemd/lib/systemd/systemd systemd.unified_cgroup_hierarchy=0";
-
-    # Overrides for <nixpkgs/nixos/modules/virtualisation/container-config.nix>
-    documentation.enable = mkOverride 500 true;
-    documentation.nixos.enable = mkOverride 500 true;
-    networking.useHostResolvConf = mkOverride 500 false;
-    services.openssh.startWhenNeeded = mkOverride 500 false;
-
-    # Bring up the network, /ifcfg.{add,del} are supplied by the vpsAdminOS host
-    systemd.services.networking-setup = {
-      description = "Load network configuration provided by the vpsAdminOS host";
-      before = [ "network.target" ];
-      wantedBy = [ "network.target" ];
-      after = [ "network-pre.target" ];
-      path = [ pkgs.iproute2 ];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStart = "${pkgs.bash}/bin/bash /ifcfg.add";
-        ExecStop = "${pkgs.bash}/bin/bash /ifcfg.del";
-      };
-      unitConfig.ConditionPathExists = "/ifcfg.add";
-      restartIfChanged = false;
-    };
-  };
-
   config = { config, pkgs, lib, ... }: {
     system.stateVersion = "23.11";
+
+    imports = [
+      inputs.secrets.kulich
+      inputs.home-manager-2511.nixosModules.home-manager
+      inputs.nixos-mailserver-2511.nixosModules.default
+      inputs.svatba.nixosModules.default
+      self.nixosModules.vpsfree
+    ];
 
     nix = {
       package = pkgs.nix;
@@ -90,14 +28,16 @@ let
       443
       587  # SMPT
       993  # IMAP
-      8998 # temporary
     ];
 
     services.openssh.enable = true;
     services.openssh.settings.PermitRootLogin = "yes";
 
+    # === Mailserver ===
+
     mailserver = {
       enable = true;
+      stateVersion = 3;
       fqdn = "mail.wohlrath.cz";
       domains = [ "wohlrath.cz" ];
       certificateScheme = "acme-nginx";
@@ -113,22 +53,8 @@ let
       '';
     };
 
-    # services.radicale = {
-    #   enable = true;
-    #   config =
-    #   let mailAccounts = config.mailserver.loginAccounts;
-    #       htpasswd = with lib; pkgs.writeText "radicale.users" (concatStrings
-    #         (flip mapAttrsToList mailAccounts (mail: user:
-    #           mail + ":" + user.hashedPassword + "\n"
-    #         ))  # there is no user.hashedPassword, only hashedPasswordFile
-    #       );
-    #   in ''
-    #     [auth]
-    #     type = htpasswd
-    #     htpasswd_filename = ${htpasswd}
-    #     htpasswd_encryption = bcrypt
-    #   '';
-    # };
+    
+    # === Web ===
 
     security.acme.acceptTerms = true;
 
@@ -143,15 +69,6 @@ let
             try_files $uri $uri/ /index.html;
           '';
         };
-        #"vladislav.wohlrath.cz" = {
-        #  forceSSL = true;
-        #  enableACME = true;
-        #  serverAliases = [ "www.vladislav.wohlrath.cz" ];
-        #  root = "${inputs.homepage.packages.x86_64-linux.default}/html/";
-        #  extraConfig = ''
-        #    index index.html;
-        #  '';
-        #};
         "wohlrath.cz" = {
           forceSSL = true;
           enableACME = true;
@@ -160,52 +77,33 @@ let
             root = "/var/www/wohlrath.cz";
           };
         };
-        "dav.wohlrath.cz" = {
-          forceSSL = true;
-          enableACME = true;
-          locations."/" = {
-            proxyPass = "http://localhost:5232/";
-            extraConfig = ''
-              proxy_set_header  X-Script-Name /;
-              proxy_set_header  X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_pass_header Authorization;
-            '';
-          };
-        };
-        "grafana.wohlrath.cz" = {
-          forceSSL = true;
-          enableACME = true;
-          locations."/" = {
-            proxyPass = "http://${toString config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port}";
-            proxyWebsockets = true;
-            recommendedProxySettings = true;
-          };
-        };
       };
     };
 
-    services.postgresql = {
-      enable = true;
-      ensureDatabases = [ "data" ];
-      ensureUsers = [
-        { name = "kulich-api"; }
-        { name = "vladidobro"; }
-      ];
-      authentication = lib.mkOverride 10 ''
-        # type database DBUser optional_identmap
-        local all all peer map=usermap
-      '';
-      identMap = ''
-        # arbitraryMapName systemUser DBUser
-        usermap      root       postgres
-        usermap      postgres   postgres
-        usermap      vladidobro postgres
-        usermap      /^(.*)$    \1
-      '';
+    services.svatba = {
+      enable = true;   
+      backendHost = "api.svatba.maskova.wohlrath.cz";
+      frontendHost = "svatba.maskova.wohlrath.cz";
+      mongoUri = "mongodb://127.0.0.1:27017/svatba";
     };
 
-    services.redis.servers.redis = {
+    # === Databases ===
+
+    services.postgresql = {
       enable = true;
+      authentication = lib.mkOverride 10 
+      # type database DBUser optional_identmap
+      ''
+        local all all peer map=usermap
+      '';
+      identMap = 
+      # mapName systemUser DBUser
+      ''
+        usermap   root       postgres
+        usermap   postgres   postgres
+        usermap   vladidobro postgres
+        usermap   /^(.*)$    \1
+      '';
     };
 
     services.mongodb = {
@@ -215,9 +113,35 @@ let
       in pkgs.mongodb-ce;
     };
 
+    # === Users ===
+
+    environment.systemPackages = with pkgs; [
+      mongosh
+    ];
+
+    home-manager.sharedModules = [
+      self.homeModules.default
+    ];
     home-manager = {
       useUserPackages = true;
       useGlobalPkgs = true;
+    };
+
+    users.users.vladidobro = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+    };
+    home-manager.users.vladidobro = {
+      home.stateVersion = "23.11";
+      home.username = "vladidobro";
+      home.homeDirectory = "/home/vladidobro";
+      vladidobro = {
+        enable = true;
+        minimal = true;
+        basic = true;
+        aliases = true;
+        nvim.enable = true;
+      };
     };
 
     users.users.daniel = {
@@ -240,58 +164,11 @@ let
       extraGroups = [ "podman" ];
     };
 
-    users.users.vladidobro = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ];
-    };
-
-    services.grafana = {
-      enable = true;
-      settings = {
-        server = {
-          http_addr = "127.0.0.1";
-          http_port = 3050;
-          domain = "grafana.wohlrath.cz";
-          serve_from_sub_path = true;
-        };
-      };
-    };
-
-    services.svatba = {
-      enable = true;   
-      backendHost = "api.svatba.maskova.wohlrath.cz";
-      frontendHost = "svatba.maskova.wohlrath.cz";
-      mongoUri = "mongodb://127.0.0.1:27017/svatba";
-      dashboard = {
-        enable = true;
-        host = "widgets.svatba.maskova.wohlrath.cz";
-        mongoUri = "mongodb://127.0.0.1:27017/svatba";
-      };
-    };
-
-    environment.systemPackages = with pkgs; [
-      mongosh
-    ];
-
-    home-manager.users.vladidobro = home;
-
-    imports = [
-      inputs.secrets.kulich
-      inputs.home-manager-2405.nixosModules.home-manager
-      inputs.agenix-2405.nixosModules.default
-      inputs.nixos-mailserver-2405.nixosModules.default
-      inputs.svatba.nixosModules.default
-      inputs.svatba-dashboard.nixosModules.default
-    ];
-
-    home-manager.sharedModules = [
-      self.homeModules.default
-    ];
   };
 
 in { 
-  flake.nixosConfigurations.kulich = inputs.nixpkgs-2405.lib.nixosSystem {
+  flake.nixosConfigurations.kulich = inputs.nixpkgs-2511.lib.nixosSystem {
     system = "x86_64-linux";
-    modules = [ config vpsfree ];
+    modules = [ config ];
   };
 }
