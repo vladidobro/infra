28180276a76ac9111c0d2da5e4be4e942e9f67aa
Vladislav Wohlrath <vladislav@wohlrath.cz>
2026-03-10 13:56:41 +0100

    dev

--- hosts/sf.nix
+++ hosts/sf.nix
@@ -4,22 +4,88 @@ let
   nix-darwin = inputs.nix-darwin-2511;
   home-manager = inputs.home-manager-2511;
   nixvim = inputs.nixvim-2511;
-  secrets = inputs.secrets.sf;
 
+  home = { pkgs, ... }: 
+  {
+    home.stateVersion = "25.11";
+    home.username = "vladislavwohlrath";
+    home.homeDirectory = "/Users/vladislavwohlrath";
 
-  config = { pkgs, ... }: {
-    system.stateVersion = 6;  # 25.11
+    vladidobro = {
+      enable = true;
+      aliases = true;
+      minimal = true;
+      basic = true;
+      full = true;
+      graphical = true;
+      nvim.enable = false;
+    };
 
-    imports = [
-      home-manager.darwinModules.home-manager
-      secrets
+    home.sessionPath = [
+      "$HOME/.local/bin"
     ];
 
-    # === System ===
+    home.shellAliases = {
+      v = ". ~/venv/bin/activate";
+      V = "deactivate";
+    };
 
-    system.primaryUser = "vladislavwohlrath";
+    home.packages = with pkgs; [
+      nixos-rebuild
+      #qemu
+      #inetutils
+      #renameutils
+      #pyright
+      #poetry
+      #(python3.withPackages (ps: with ps; [ pip ]))
+      #ruff
+    ];
+
+    programs.ssh.enableDefaultConfig = false;
+    programs.ssh.matchBlocks = {
+      "*" = {
+        forwardAgent = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        compression = false;
+        addKeysToAgent = "no";
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+      "kulich" = {
+        user = "vladidobro";
+        hostname = "wohlrath.cz";
+      };
+    };
+
+    programs.git.ignores = [ ".envrc" ".direnv" ];
+    programs.git.settings = {
+      gpg.format = "ssh";
+      user.signingkey = "~/.ssh/id_ed25519";
+      commit.gpgsign = true;
+    };
+
+    programs.nixvim.enable = true;
+    programs.nixvim.imports = [ self.nixvimModules.default ];
+  };
+
+  config = { pkgs, ... }: {
+    imports = [
+      home-manager.darwinModules.home-manager
+    ];
+
+    system.stateVersion = 6;  # 25.11
     nixpkgs.hostPlatform = "aarch64-darwin";
     nixpkgs.config.allowUnfree = true;
+
+    fonts.packages = with pkgs; [
+      nerd-fonts.noto
+    ];
+    system.primaryUser = "vladislavwohlrath";
     nix = {
       settings.trusted-users = [ "vladislavwohlrath" ];
       settings.experimental-features = [ "nix-command" "flakes" ];
@@ -27,9 +93,7 @@ let
         nixpkgs.flake = nixpkgs;
       };
     };
-    fonts.packages = with pkgs; [ nerd-fonts.noto ];
-
-    # === Packages ===
+    #nixpkgs.overlays = [ nixvim.overlays.default ];
 
     programs.bash.enable = true;
     programs.zsh.enable = true;
@@ -41,25 +105,25 @@ let
       enable = true;
       brews = [
         "pipx"
-        "azure-cli"
-        "kubectl"
-        "kubelogin"
-        "colima"
-        "uv"
-        "k9s"
-        "argocd"
-        "glab"
-        #"msodbcsql18"
+	"azure-cli"
+	"kubectl"
+	"kubelogin"
+	"colima"
+	"docker"
+	"qemu"
+	"uv"
+	"k9s"
+	"argocd"
+	"glab"
+	#"msodbcsql18"
       ];
+      #taps = [
+      #  { name = "microsoft/mssql-release"; clone_target="https://github.com/Microsoft/homebrew-mssql-release"; }
+      #];
     };
-    environment.systemPackages = with pkgs; [ 
-      git
-      tmux
-      neovim
-    ];
 
-    # === Users ===
-    
+    users.users.vladislavwohlrath.home = "/Users/vladislavwohlrath";
+
     home-manager = {
       useUserPackages = true;
       useGlobalPkgs = true;
@@ -68,73 +132,17 @@ let
       nixvim.homeModules.nixvim
       self.homeModules.default
     ];
+    home-manager.users.vladislavwohlrath = home;
 
-    users.users.vladislavwohlrath.home = "/Users/vladislavwohlrath";
-    home-manager.users.vladislavwohlrath = { pkgs, ... }:
-    {
-      home.stateVersion = "25.11";
-      home.username = "vladislavwohlrath";
-      home.homeDirectory = "/Users/vladislavwohlrath";
-
-      vladidobro = {
-        enable = true;
-        aliases = true;
-        minimal = true;
-        basic = true;
-        full = true;
-        graphical = true;
-        nvim.enable = false;
-      };
-
-      home.sessionPath = [
-        "$HOME/.local/bin"
-      ];
-
-      home.shellAliases = {
-        v = ". ~/venv/bin/activate";
-        V = "deactivate";
-      };
-
-      home.packages = with pkgs; [
-        nixos-rebuild
-      ];
-
-      programs.ssh.enableDefaultConfig = false;
-      programs.ssh.matchBlocks = {
-        "*" = {
-          forwardAgent = false;
-          serverAliveInterval = 0;
-          serverAliveCountMax = 3;
-          compression = false;
-          addKeysToAgent = "no";
-          hashKnownHosts = false;
-          userKnownHostsFile = "~/.ssh/known_hosts";
-          controlMaster = "no";
-          controlPath = "~/.ssh/master-%r@%n:%p";
-          controlPersist = "no";
-          identityFile = "~/.ssh/id_ed25519";
-        };
-        "kulich" = {
-          user = "vladidobro";
-          hostname = "wohlrath.cz";
-        };
-      };
-
-      programs.git.ignores = [ ".envrc" ".direnv" ];
-      programs.git.settings = {
-        gpg.format = "ssh";
-        user.signingkey = "~/.ssh/id_ed25519";
-        commit.gpgsign = true;
-      };
-
-      programs.nixvim.enable = true;
-      programs.nixvim.imports = [ self.nixvimModules.default ];
-    };
+    environment.systemPackages = with pkgs; [ 
+      vim
+      git
+    ];
   };
 
 in {
   flake.darwinConfigurations.sf = nix-darwin.lib.darwinSystem {
     system = "aarch64-darwin";
-    modules = [ config ];
+    modules = [ config inputs.secrets.sf ];
   };
 }
