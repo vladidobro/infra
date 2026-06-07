d89a5bc807a1316b476a59e1beffc7e51085e014
Vladislav Wohlrath <vladislav@wohlrath.cz>
2026-06-07 13:19:49 +0200

    xf86 binds pipewire

--- hosts/myskus.nix
+++ hosts/myskus.nix
@@ -188,15 +188,10 @@ let
 	    "XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
 
 	    # Volume (using pactl for PipeWire/PulseAudio)
-	    "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
-	    "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
-	    "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
-	    "XF86AudioMicMute" = "exec pactl set-source-mute @DEFAULT_SOURCE@ toggle";
-
-	    # Media controls
-	    "XF86AudioPlay" = "exec playerctl play-pause";
-	    "XF86AudioNext" = "exec playerctl next";
-	    "XF86AudioPrev" = "exec playerctl previous";
+	    "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+";
+	    "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
+	    "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+	    "XF86AudioMicMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
 	  };
 	};
       };
